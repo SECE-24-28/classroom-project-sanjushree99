f2a01c1c387f0572dea1e7327a2060efcfe0a461
sanjushree99 <sanjushreej99@gmail.com>
2025-12-12 21:00:19 +0530

    Add files via upload

---
+++ Day10/controllers/studentController.js
@@ -0,0 +1,11 @@
+const Student = require("../models/student");
+exports.createStudent = async (req, res) => {
+    try {
+        const student = await Student.create(req.body);
+        res.json(student);
+    } catch (err) {
+        res.status(400).json({
+            error: err.message
+        });
+    }
+};
